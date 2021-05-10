7a1d5c827a9a5b3911ecf06765309bbd4a4315e6
Ange Carelle <tankeua@3il.fr>
2021-05-10 16:18:48 +0200

    Envoie des classes Collecte et DaoCollecte

---
+++ src/projet/dao/DaoCollecte.java
@@ -0,0 +1,222 @@
+package projet.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import jfox.jdbc.UtilJdbc;
+import projet.data.Collecte;
+
+
+public class DaoCollecte {
+
+	
+	// Champs
+
+	@Inject
+	private DataSource		dataSource;
+	@Inject
+	private DaoSite_de_collecte daoSite_de_collecte;
+
+	
+	// Actions
+
+	public int inserer( Collecte collecte ) {
+
+		Connection			cn		= null;
+		PreparedStatement	stmt	= null;
+		ResultSet 			rs		= null;
+		String				sql;
+
+		try {
+			cn = dataSource.getConnection();
+			sql = "INSERT INTO collecte ( qte_collation, date_debut, date_fin, nbre_infirmiers, nbre_medecins, nbre_secretaire, nbre_agents_collation, horaire_debut, horaire_fin, id_site ) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
+			stmt = cn.prepareStatement( sql, Statement.RETURN_GENERATED_KEYS );
+			stmt.setObject( 1, collecte.getQte_collation());
+			stmt.setObject( 2, collecte.getDate_debut());
+			stmt.setObject( 3, collecte.getDate_fin());
+			stmt.setObject( 4, collecte.getNbre_infirmiers());
+			stmt.setObject( 5, collecte.getNbre_medecins());
+			stmt.setObject( 6, collecte.getNbre_secretaire());
+			stmt.setObject( 7, collecte.getNbre_agents_collation());
+			stmt.setObject( 8, collecte.getHoraire_debut());
+			stmt.setObject( 9, collecte.getHoraire_fin());
+			stmt.setObject( 10, collecte.getSite_de_collecte().getId_site_de_collecte());	
+			stmt.executeUpdate();
+
+			// Récupère l'identifiant généré par le SGBD
+			rs = stmt.getGeneratedKeys();
+			rs.next();
+			collecte.setId_collecte( rs.getObject( 1, Integer.class) );
+			
+	
+		} catch ( SQLException e ) {
+			throw new RuntimeException(e);
+		} finally {
+			UtilJdbc.close( rs, stmt, cn );
+		}
+		
+		return collecte.getId_collecte();
+	}
+
+
+	public void modifier( Collecte collecte ) {
+
+		Connection			cn		= null;
+		PreparedStatement	stmt	= null;
+		String				sql;
+
+		try {
+			cn = dataSource.getConnection();
+			sql = "UPDATE collecte SET qte_collation= ? , date_debut= ?, date_fin= ?, nbre_infirmiers= ?, nbre_medecins= ?, nbre_secretaire=?, nbre_agents_collation= ?, horaire_debut= ?, horaire_fin=?, id_site = ? WHERE idcollecte = ?";
+			stmt = cn.prepareStatement( sql );
+			stmt.setObject( 1, collecte.getQte_collation());
+			stmt.setObject( 2, collecte.getDate_debut());
+			stmt.setObject( 3, collecte.getDate_fin());
+			stmt.setObject( 4, collecte.getNbre_infirmiers());
+			stmt.setObject( 5, collecte.getNbre_medecins());
+			stmt.setObject( 6, collecte.getNbre_secretaire());
+			stmt.setObject( 7, collecte.getNbre_agents_collation());
+			stmt.setObject( 8, collecte.getHoraire_debut());
+			stmt.setObject( 9, collecte.getHoraire_fin());
+			stmt.setObject( 10, collecte.getSite_de_collecte().getId_site_de_collecte());
+			stmt.setObject( 11, collecte.getId_collecte());
+			stmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			UtilJdbc.close( stmt, cn );
+		}
+	}
+
+
+	public void supprimer( int id_collecte ) {
+
+		Connection			cn 		= null;
+		PreparedStatement	stmt 	= null;
+		String				sql;
+
+		try {
+			cn = dataSource.getConnection();
+			sql = "DELETE FROM collecte WHERE idcollecte = ? ";
+			stmt = cn.prepareStatement( sql );
+			stmt.setObject( 1, id_collecte );
+			stmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			UtilJdbc.close( stmt, cn );
+		}
+	}
+
+	
+	public Collecte retrouver( int id_collecte ) {
+
+		Connection			cn 		= null;
+		PreparedStatement	stmt	= null;
+		ResultSet 			rs		= null;
+		String				sql;
+
+		try {
+			cn = dataSource.getConnection();
+			sql = "SELECT * FROM collecte WHERE id_collecte = ?";
+			stmt = cn.prepareStatement( sql );
+			stmt.setObject(1, id_collecte);
+			rs = stmt.executeQuery();
+
+			if ( rs.next() ) {
+				return construireCollecte( rs, true );
+			} else {
+				return null;
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			UtilJdbc.close( rs, stmt, cn );
+		}
+	}
+
+
+	public List<Collecte> listerTout() {
+
+		Connection			cn 		= null;
+		PreparedStatement	stmt 	= null;
+		ResultSet 			rs		= null;
+		String				sql;
+
+		try {
+			cn = dataSource.getConnection();
+			sql = "SELECT * FROM collecte ORDER BY id_collecte";
+			stmt = cn.prepareStatement( sql );
+			rs = stmt.executeQuery();
+
+			List<Collecte> collectes = new ArrayList<>();
+			while (rs.next()) {
+				collectes.add( construireCollecte( rs, false ) );
+			}
+			return collectes;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			UtilJdbc.close( rs, stmt, cn );
+		}
+	}
+	
+	  public int compterPourSite( int id_site ) {
+	    	
+			Connection			cn		= null;
+			PreparedStatement	stmt 	= null;
+			ResultSet 			rs		= null;
+
+			try {
+				cn = dataSource.getConnection();
+	            String sql = "SELECT COUNT(*) FROM personne WHERE id_site = ?";
+	            stmt = cn.prepareStatement( sql );
+	            stmt.setObject( 1, id_site );
+	            rs = stmt.executeQuery();
+
+	            rs.next();
+	            return rs.getInt( 1 );
+
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			} finally {
+				UtilJdbc.close( rs, stmt, cn );
+			}
+	    }
+	
+	
+	// Méthodes auxiliaires
+	
+	private Collecte construireCollecte( ResultSet rs,  boolean flagComplet ) throws SQLException {
+		Collecte collecte = new Collecte();
+		collecte.setId_collecte( rs.getObject( "id_collecte", Integer.class ) );
+		collecte.setQte_collation( rs.getObject( "qte_collation", Integer.class ) );
+		collecte.setDate_debut(rs.getObject("date_debut", LocalDate.class ) );
+		collecte.setDate_fin(rs.getObject("date_fin", LocalDate.class ) );
+		collecte.setNbre_infirmiers( rs.getObject( "nbre_infirmiers", Integer.class ) );
+		collecte.setNbre_medecins( rs.getObject( "nbre_medecins", Integer.class ) );
+		collecte.setNbre_secretaire(rs.getObject("nbre_secretaire", Integer.class ));
+		collecte.setNbre_agents_collation(rs.getObject("nbre_agents_collation", Integer.class ));
+		collecte.setHoraire_debut(rs.getObject("horaire_debut", LocalTime.class ) );
+		collecte.setHoraire_fin(rs.getObject("horaire_fin", LocalTime.class ) );
+		if ( flagComplet ) {
+			collecte.setSite_de_collecte( daoSite_de_collecte.retrouver( rs.getObject("id_site", Integer.class) ) );
+		}
+
+		return collecte;
+	}
+
+}
---
+++ src/projet/data/Collecte.java
@@ -0,0 +1,246 @@
+package projet.data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+import org.exolab.castor.types.DateTime;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+public class Collecte  {
+	
+
+	// Données observables
+	
+	private final Property<Integer>	id_collecte		= new SimpleObjectProperty<>();
+	private final Property<Integer>	qte_collation	= new SimpleObjectProperty<>();
+	private final Property<LocalDate>	date_debut	= new SimpleObjectProperty<>();
+	private final Property<LocalDate>	date_fin	= new SimpleObjectProperty<>();
+	private final Property<Integer>	nbre_infirmiers	= new SimpleObjectProperty<>();
+	private final Property<Integer>	nbre_medecins	= new SimpleObjectProperty<>();
+	private final Property<Integer> nbre_secretaire	= new SimpleObjectProperty<>();
+	private final Property<Integer>	nbre_agents_collation	= new SimpleObjectProperty<>();
+	private final Property<LocalTime>	horaire_debut	= new SimpleObjectProperty<>();
+	private final Property<LocalTime>	horaire_fin	= new SimpleObjectProperty<>();
+	private final Property<Site_de_collecte>	Site_de_collecte		= new SimpleObjectProperty<>();
+	
+	
+	// Constructeurs
+	
+	public Collecte() {
+	}
+
+	public Collecte( final int id_collecte, final int qte_collation, final LocalDate date_debut, final LocalDate date_fin, final int nbre_infirmiers, final int nbre_medecins, final int nbre_secretaire, final int nbre_agents_collation, final LocalTime horaire_debut, final LocalTime horaire_fin, final Site_de_collecte site_de_collecte) {
+		setId_collecte(id_collecte);
+		setQte_collation(qte_collation);
+		setDate_debut(date_debut);
+		setDate_fin(date_fin);
+		setNbre_infirmiers(nbre_infirmiers);
+		setNbre_medecins(nbre_medecins);
+		setNbre_secretaire(nbre_secretaire);
+		setNbre_agents_collation(nbre_agents_collation);
+		setHoraire_debut(horaire_debut);
+		setHoraire_fin(horaire_fin);
+		setSite_de_collecte(site_de_collecte);
+	}
+	
+	
+	
+	
+	// Getters et Setters
+
+	public final Property<Integer> id_collecteProperty() {
+		return this.id_collecte;
+	}
+	
+
+	public final Integer getId_collecte() {
+		return this.id_collecteProperty().getValue();
+	}
+	
+
+	public final void setId_collecte(final Integer id_collecte) {
+		this.id_collecteProperty().setValue(id_collecte);
+	}
+	
+
+	public final Property<Integer> qte_collationProperty() {
+		return this.qte_collation;
+	}
+	
+
+	public final Integer getQte_collation() {
+		return this.qte_collationProperty().getValue();
+	}
+	
+
+	public final void setQte_collation(final Integer qte_collation) {
+		this.qte_collationProperty().setValue(qte_collation);
+	}
+	
+
+	public final Property<LocalDate> date_debutProperty() {
+		return this.date_debut;
+	}
+	
+
+	public final LocalDate getDate_debut() {
+		return this.date_debutProperty().getValue();
+	}
+	
+
+	public final void setDate_debut(final LocalDate date_debut) {
+		this.date_debutProperty().setValue(date_debut);
+	}
+	
+
+	public final Property<LocalDate> date_finProperty() {
+		return this.date_fin;
+	}
+	
+
+	public final LocalDate getDate_fin() {
+		return this.date_finProperty().getValue();
+	}
+	
+
+	public final void setDate_fin(final LocalDate date_fin) {
+		this.date_finProperty().setValue(date_fin);
+	}
+	
+
+	public final Property<Integer> nbre_infirmiersProperty() {
+		return this.nbre_infirmiers;
+	}
+	
+
+	public final Integer getNbre_infirmiers() {
+		return this.nbre_infirmiersProperty().getValue();
+	}
+	
+
+	public final void setNbre_infirmiers(final Integer nbre_infirmiers) {
+		this.nbre_infirmiersProperty().setValue(nbre_infirmiers);
+	}
+	
+
+	public final Property<Integer> nbre_medecinsProperty() {
+		return this.nbre_medecins;
+	}
+	
+
+	public final Integer getNbre_medecins() {
+		return this.nbre_medecinsProperty().getValue();
+	}
+	
+
+	public final void setNbre_medecins(final Integer nbre_medecins) {
+		this.nbre_medecinsProperty().setValue(nbre_medecins);
+	}
+	
+
+	public final Property<Integer> nbre_secretaireProperty() {
+		return this.nbre_secretaire;
+	}
+	
+
+	public final Integer getNbre_secretaire() {
+		return this.nbre_secretaireProperty().getValue();
+	}
+	
+
+	public final void setNbre_secretaire(final Integer nbre_secretaire) {
+		this.nbre_secretaireProperty().setValue(nbre_secretaire);
+	}
+	
+
+	public final Property<Integer> nbre_agents_collationProperty() {
+		return this.nbre_agents_collation;
+	}
+	
+
+	public final Integer getNbre_agents_collation() {
+		return this.nbre_agents_collationProperty().getValue();
+	}
+	
+
+	public final void setNbre_agents_collation(final Integer nbre_agents_collation) {
+		this.nbre_agents_collationProperty().setValue(nbre_agents_collation);
+	}
+	
+
+	public final Property<LocalTime> horaire_debutProperty() {
+		return this.horaire_debut;
+	}
+	
+
+	public final LocalTime getHoraire_debut() {
+		return this.horaire_debutProperty().getValue();
+	}
+	
+
+	public final void setHoraire_debut(final LocalTime horaire_debut) {
+		this.horaire_debutProperty().setValue(horaire_debut);
+	}
+	
+
+	public final Property<LocalTime> horaire_finProperty() {
+		return this.horaire_fin;
+	}
+	
+
+	public final LocalTime getHoraire_fin() {
+		return this.horaire_finProperty().getValue();
+	}
+	
+
+	public final void setHoraire_fin(final LocalTime horaire_fin) {
+		this.horaire_finProperty().setValue(horaire_fin);
+	}
+	
+	public final Property<Site_de_collecte> site_de_collecteProperty() {
+		return this.Site_de_collecte;
+	}
+
+	public final projet.data.Site_de_collecte getSite_de_collecte() {
+		return this.site_de_collecteProperty().getValue();
+	}
+
+	public final void setSite_de_collecte(final projet.data.Site_de_collecte site_de_collecte) {
+		this.site_de_collecteProperty().setValue(site_de_collecte);
+	}
+
+
+	
+	
+	// hashCode() & equals()
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id_collecte.getValue() );
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Collecte other = (Collecte) obj;
+		return Objects.equals(id_collecte.getValue(), other.id_collecte.getValue() );
+	}
+
+	
+	
+
+	
+	
+}
+
